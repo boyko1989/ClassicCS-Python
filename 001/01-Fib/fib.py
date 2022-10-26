aeabbd88ae9bdcd5d468bdcbbaa04be876191793
boyko1989 <hunwiss89@gmail.com>
2022-10-26 18:32:03 +0400

    Begin simple Fibonacci

--- 001/01-Fib/fib.py
+++ 001/01-Fib/fib.py
@@ -1,5 +1,6 @@
 from typing import Dict
-memo: Dict[int, int] = {0:0,1:1}
+
+memo: Dict[int, int] = {0: 0, 1: 1}
 
 
 def fib(n: int) -> int:
@@ -8,15 +9,23 @@ def fib(n: int) -> int:
 
     return fib(n - 1) + fib(n - 2)
 
+
 def fib_m(n: int) -> int:
     if n not in memo:
         memo[n] = fib_m(n - 1) + fib_m(n - 2)
     return memo[n]
 
 
+def fib_simple(n: int) -> int:
+    if n == 0: return n
+    last: int = 0
+    next: int = 1
+
+    for _ in range(1, n):
+        last, next = next, last + 1
+    return next
+
 # print(fib_m(3))
 
 for i in range(1, 9):
-     print(fib_m(i))
-
-
+    print(fib_simple(i))
