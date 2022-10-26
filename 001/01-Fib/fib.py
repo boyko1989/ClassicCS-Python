39ad4a0f2e1b063d6a24a3fcd0a53a0d5c545271
boyko1989 <hunwiss89@gmail.com>
2022-10-26 17:59:05 +0400

    Add fuction for memoisation

--- 001/01-Fib/fib.py
+++ 001/01-Fib/fib.py
@@ -1,11 +1,22 @@
+from typing import Dict
+memo: Dict[int, int] = {0:0,1:1}
+
+
 def fib(n: int) -> int:
     if n < 2:
         return n
 
     return fib(n - 1) + fib(n - 2)
 
+def fib_m(n: int) -> int:
+    if n not in memo:
+        memo[n] = fib_m(n - 1) + fib_m(n - 2)
+    return memo[n]
 
-# print(fib(3))
+
+# print(fib_m(3))
 
 for i in range(1, 9):
-    print(fib(i))
+     print(fib_m(i))
+
+
